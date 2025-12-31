ca16b88f42e7fa7c09899b1d720f62621a3a7f48
bsivanesan-star <bsivanesan@denovosystem.com>
2025-12-31 16:03:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(10);
+
+for (let key in circle) {
+    console.log(key, circle[key]);
+}
